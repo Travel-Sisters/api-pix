f7e44c4223a1fe962faab49bc82570216ab4d358
Carlos <111136767+CarlosPossoni@users.noreply.github.com>
2024-05-27 21:19:02 -0300

    Add files via upload

---
+++ credentials/credentials.js
@@ -0,0 +1,8 @@
+module.exports = {
+	// PRODUÇÃO = false
+	// HOMOLOGAÇÃO = true
+	sandbox: true,
+	client_id: 'Client_Id_fc1e524b7661d4bd51babf4aef11dab9557c9003',
+	client_secret: 'Client_Secret_81d0231139c91201a3d76415f9d6f2b169540587',
+	certificate: 'homologacao-553292-TRAVEL-SISTERS-DESK-HOME.p12',
+}
